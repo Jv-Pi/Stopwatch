7f760dd196016813df212aefd8f4e60fa5c2b0c1
João Victor Pires Dos Santos <54391360+Jv-Pi@users.noreply.github.com>
2021-02-23 13:44:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var hh = 0;
 var mn = 0;
 var ss = 0;
 
+var counterp1 = 1;
+var counterp2 = 1;
+
 now = new Date;
 var weekday = new Array(7);
 weekday[0] = "Sunday";
@@ -26,7 +29,7 @@ month[9] = "October";
 month[10] = "November";
 month[11] = "December";
 
-var temp = 1; //1hr em milesimos
+var temp = 1000; //1hr em milesimos
 var cronometro;
 
 function start() {
@@ -45,6 +48,14 @@ function stop() {
     document.getElementById('counter').innerText = '00:00:00';
 }
 
+function p1() {
+    document.getElementById('counter-P1').innerText = counterp1++;
+}
+
+function p2() {
+    document.getElementById('counter-P2').innerText = counterp2++;
+}
+
 function timer() {
     ss++;
     if (ss == 60) {
@@ -61,4 +72,35 @@ function timer() {
     return format
 }
 
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("counter-regressive").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("counter-regressive").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
 document.getElementById('date').innerText = ("Today is " + weekday[now.getDay()] + ", " + now.getDate() + " of " + month[now.getMonth()] + " in " + now.getFullYear())
+
+document.getElementById('hr').innerText = ("Now is " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
